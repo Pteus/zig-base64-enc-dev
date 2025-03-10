c5e4524544b526323c24aeb6f3d93a20dfa6feb0
Pedro Mateus <pedropmateus91@gmail.com>
2025-03-10 21:33:16 +0000

    complete

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const stdout = std.io.getStdOut().writer();
 
 const Base64 = struct {
     _table: *const [64]u8,
@@ -13,16 +14,61 @@ const Base64 = struct {
         };
     }
 
-    pub fn _char_at(self: Base64, index: usize) u8 {
+    fn _char_at(self: Base64, index: usize) u8 {
         return self._table[index];
     }
 
-    pub fn encode(self: Base64, allocator: std.mem.Allocator, input: []const u8) ![]u8 {
+    fn _char_index(self: Base64, char: u8) u8 {
+        if (char == '=') {
+            return 64;
+        }
+
+        var index: u8 = 0;
+        for (0..63) |i| {
+            if (self._char_at(i) == char) {
+                break;
+            }
+            index += 1;
+        }
+
+        return index;
+    }
+
+    fn decode(self: Base64, allocator: std.mem.Allocator, input: []const u8) ![]u8 {
+        if (input.len == 0) {
+            return "";
+        }
+        const n_output = try _calc_decode_length(input);
+        var output = try allocator.alloc(u8, n_output);
+        var count: u8 = 0;
+        var iout: u64 = 0;
+        var buf = [4]u8{ 0, 0, 0, 0 };
+
+        for (0..input.len) |i| {
+            buf[count] = self._char_index(input[i]);
+            count += 1;
+            if (count == 4) {
+                output[iout] = (buf[0] << 2) + (buf[1] >> 4);
+                if (buf[2] != 64) {
+                    output[iout + 1] = (buf[1] << 4) + (buf[2] >> 2);
+                }
+                if (buf[3] != 64) {
+                    output[iout + 2] = (buf[2] << 6) + buf[3];
+                }
+                iout += 3;
+                count = 0;
+            }
+        }
+
+        return output;
+    }
+
+    fn encode(self: Base64, allocator: std.mem.Allocator, input: []const u8) ![]u8 {
         if (input.len == 0) {
             return "";
         }
 
-        const n_out = try _calc_decode_length(input);
+        const n_out = try _calc_encode_length(input);
         var out = try allocator.alloc(u8, n_out);
         var buf = [3]u8{ 0, 0, 0 };
         var count: u8 = 0;
@@ -60,10 +106,6 @@ const Base64 = struct {
     }
 };
 
-pub fn main() !void {
-    std.debug.print("hello world", .{});
-}
-
 fn _calc_encode_length(input: []const u8) !usize {
     if (input.len < 3) {
         const n_output: usize = 4;
@@ -82,4 +124,18 @@ fn _calc_decode_length(input: []const u8) !usize {
 
     const n_output: usize = try std.math.divFloor(usize, input.len, 4);
     return n_output * 3;
+}
+
+pub fn main() !void {
+    var memory_buffer: [1000]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&memory_buffer);
+    const allocator = fba.allocator();
+
+    const text = "Testing some more stuff";
+    const etext = "VGVzdGluZyBzb21lIG1vcmUgc3R1ZmY=";
+    const base64 = Base64.init();
+    const encoded_text = try base64.encode(allocator, text);
+    const decoded_text = try base64.decode(allocator, etext);
+    try stdout.print("Encoded text: {s}\n", .{encoded_text});
+    try stdout.print("Decoded text: {s}\n", .{decoded_text});
 }
